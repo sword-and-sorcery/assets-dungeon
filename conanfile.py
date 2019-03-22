12652dc0e16aadbe9c688f533d3d662f76bfb049
Javier G. Sogo <jgsogo@gmail.com>
2019-03-22 18:17:12 +0100

    grab tiles from internet and package them

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conans import ConanFile, tools
+
+class AssetsDungeon(ConanFile):
+    name = "assets-dungeon"
+    version = "1.0"
+    description = "Tileset for a dungeon"
+    topics = "assets", "dungeon"
+    url = "https://www.patreon.com/posts/modular-dungeon-19227322"
+
+    def source(self):
+        url = "https://www.patreon.com/file?h=19227322&i=2255154"
+        tools.get(url, filename="tileset")
+
+    def package_info(self):
+        self.cpp_info.resdirs = ["tileset",]
+
+    def package(self):
+        self.copy("board.xml", dst="tileset")
+        self.copy("*.png", dst="tileset")
+        self.copy("*.jpg", dst="tileset")
+
